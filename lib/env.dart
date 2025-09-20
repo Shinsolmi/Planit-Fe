5ac7a5fa68e99f89c7127d990da2c4b683595177
Shinsolmi <solmi2449@naver.com>
2025-09-21 05:12:12 +0900

    로그인 가능

--- lib/env.dart
+++ lib/env.dart
@@ -2,5 +2,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 
 const String _envBase = String.fromEnvironment('BASE_URL', defaultValue: '');
 
-String get baseUrl =>
-    _envBase.isNotEmpty ? _envBase : (kIsWeb ? 'https://0f8d907f90ba.ngrok-free.app' : 'http://10.0.2.2:3000');
+const bool useTunnel = true; // false면 로컬
+const _tunnel = 'https://f2c240b71ba9.ngrok-free.app';
+const _local  = 'http://10.0.2.2:3000';
+String get baseUrl => _envBase.isNotEmpty ? _envBase : (useTunnel ? _tunnel : _local);
