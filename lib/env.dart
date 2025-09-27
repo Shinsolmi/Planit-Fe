7ff42848029bfa60953cfb3f61bc83a1d65ddfb8
Shinsolmi <solmi2449@naver.com>
2025-09-28 01:56:17 +0900

    내일정으로 담기 수정

--- lib/env.dart
+++ lib/env.dart
@@ -3,10 +3,8 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 const String _envBase = String.fromEnvironment('BASE_URL', defaultValue: '');
 
 
-
 const bool useTunnel =true; //false면 로컬
 const _tunnel = 'https://e49b5104edf7.ngrok-free.app';
-
 const _local  = 'http://10.0.2.2:3000';
 String get baseUrl => _envBase.isNotEmpty ? _envBase : (useTunnel ? _tunnel : _local);
 
